9c3b969d5b2c12db960ad790f9b787d87b368b27
Dexter Dagorne <dj.dagorne@gmail.com>
2020-02-10 10:54:50 -0500

    cleaning up for deploy

--- src/app.js
+++ src/app.js
@@ -27,37 +27,27 @@ app.use('/api/comments', commentsRouter)
 app.use('/api/images', imagesRouter)
 app.use('/api/auth', authRouter) //TODO compare thingful-client to tennit-client and get auth working on tennit-client
 
-// const logger = winston.createLogger({
-//     level:'info',
-//     format: winston.format.json(),
-//     transports: [
-//         new winston.transports.File({ filename: 'info.log' })
-//     ]
-// });
-// app.use(function errorHandler(error, req, res, next) {
-//     let response
-//     if (NODE_ENV === 'production') {
-//         logger.add(new winston.transports.Console({
-//             format: winston.format.simple()
-//         }))
-//         response = { error: {message: 'server error'} }; 
-//     } else {
-//         console.error(error);
-//         response = { message: error.message, error }
-//     }
-//     res.status(500).json(response);
-// })
+const logger = winston.createLogger({
+    level:'info',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.File({ filename: 'info.log' })
+    ]
+});
 
-// app.use(function validateBearerToken(req, res, next) {
-//     const apiToken = process.env.API_TOKEN
-//     const authToken = req.get('Authorization')
-  
-//     if (!authToken || authToken.split(' ')[1] !== apiToken) {
-//       return res.status(401).json({ error: 'Unauthorized request' })
-//     }
-//     // move to the next middleware
-//     next()
-//   })
+app.use(function errorHandler(error, req, res, next) {
+    let response
+    if (NODE_ENV === 'production') {
+        logger.add(new winston.transports.Console({
+            format: winston.format.simple()
+        }))
+        response = { error: {message: 'server error'} }; 
+    } else {
+        console.error(error);
+        response = { message: error.message, error }
+    }
+    res.status(500).json(response);
+})
 
 
 
