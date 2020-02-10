bdf27a8754d7f11f77ef64458ea443ee590d39d0
Dexter Dagorne <dj.dagorne@gmail.com>
2020-02-10 11:50:20 -0500

    heroku package.json updates etc

--- postgrator-config.js
+++ postgrator-config.js
@@ -6,4 +6,5 @@ module.exports = {
   "connectionString": (process.env.NODE_ENV === 'test')
     ? process.env.REACT_APP_TEST_DATABASE_URL
     : process.env.REACT_APP_DATABASE_URL,
+    "ssl": !!process.env.SSL,
 }
